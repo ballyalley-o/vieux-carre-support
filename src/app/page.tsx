8963f9c1b783685ef62a41d43971c003d892cd5e
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-01 11:16:22 +1000

    [enhance]: enhance HomePage layout with logo and app name display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import Image from 'next/image'
+import { GLOBAL } from 'vcs'
+import { ASSET_DIR } from 'vcs.dir'
+
 const HomePage = () => {
-  return ( <h1 className={"text-8xl"}>Hey</h1> );
+  return (
+    <main className={'flex flex-col text-center items-center justify-center min-h-screen px-4'}>
+      <Image src={ASSET_DIR.LOGO.svg} alt="logo" fill priority />
+      <span className="hidden lg:block ml-3">{GLOBAL.APP_NAME}</span>
+    </main>
+  )
 }
 
 export default HomePage
