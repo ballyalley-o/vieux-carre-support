d745ae1b1b7c832d63d4faf58e621b237e24245a
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-01 11:18:04 +1000

    [dir]: add asset and path directory configuration

---
+++ src/config/dir/asset.ts
@@ -0,0 +1,8 @@
+import { combine } from 'lib/utility'
+
+export const ASSET_DIR = {
+  LOGO: {
+    svg: combine('image', 'svg', 'vieux-carre-support.svg'),
+    png: combine('image', 'raster', 'vieux-carre-support.png')
+  }
+}
