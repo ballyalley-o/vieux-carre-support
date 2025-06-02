b9db03471d97ce962ba019b807888b5d9b8b6fd7
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-02 22:00:28 +1000

    [feat]: create New Ticket Page component to render the New Ticket Form

---
+++ src/app/(root)/ticket/new/page.tsx
@@ -0,0 +1,11 @@
+import NewTicketForm  from "app/(root)/ticket/new/new-ticket-form"
+
+const NewTicketPage = () => {
+  return (
+    <div className={'min-h-screen flex items-center justify-center px-4'}>
+      <NewTicketForm />
+    </div>
+  )
+}
+
+export default NewTicketPage
