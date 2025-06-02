2a52ae4f9fd4cc1ed33a12ecfe0e4117b7cf4147
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-02 22:03:38 +1000

    [dir]: define PATH_DIR for ticket routing with base URL

--- src/config/dir/path.ts
+++ src/config/dir/path.ts
@@ -1,1 +1,14 @@
-export const PATH_DIR = {}
+import { GLOBAL } from 'vcs'
+import { combine } from "lib/utility"
+
+const TICKET = 'ticket'
+const NEW    = 'new'
+
+export const _base = GLOBAL.SERVER_URL
+
+export const PATH_DIR = {
+  TICKET: {
+    root: combine(TICKET),
+    new : combine(TICKET, NEW)
+  }
+}
