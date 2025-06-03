da6f286eb05a6174117229be05fddf7d54be10d0
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-03 16:23:29 +1000

    [refactor]: enhance priority selection with color coding and value mapping

--- src/app/(root)/ticket/new/new-ticket-form.tsx
+++ src/app/(root)/ticket/new/new-ticket-form.tsx
@@ -3,18 +3,20 @@
 import { useState } from "react"
 import { FaAngleDown, FaAngleUp, FaCircle } from "react-icons/fa"
 import { transl } from 'lib/utility/translate'
+import { PRIORITY } from "lib/constant"
+import { cn } from "lib/utility"
 
 
 const NewTicketForm = () => {
     const [selectedPriority, setSelectedPriority] = useState<PriorityKeyType>('low')
     const [showDropdown, setShowDropdown]         = useState(false)
     type  PriorityKeyType                         = 'low' | 'medium' | 'high'
-    type  PriortySelectType                       = { id: number; name: PriorityKeyType }[]
-    const priority: PriortySelectType             = [
-            { id: 0, name: 'low' },
-            { id: 1, name: 'medium' },
-            { id: 2, name: 'high' }
-        ]
+    type  PriortySelectType                       = { id: number; name: PriorityKeyType; color: string, value: PRIORITY }[]
+    const priority: PriortySelectType = [
+      { id: 0, name: 'low', color: 'text-priority-low', value: PRIORITY.LOW },
+      { id: 1, name: 'medium', color: 'text-priority-medium', value: PRIORITY.MEDIUM },
+      { id: 2, name: 'high', color: 'text-priority-high', value: PRIORITY.HIGH }
+    ]
 
     const dropdownIcon = showDropdown ?  <FaAngleUp/> : <FaAngleDown/>
     return (
@@ -27,16 +29,16 @@ const NewTicketForm = () => {
           <div className="relative">
             <button type="button" className={"w-full border p-3 input-default flex justify-between items-center"} onClick={() => setShowDropdown(!showDropdown)}>
               <span className={"flex items-center gap-2"}>
-                <FaCircle className={`text-xs ${selectedPriority === 'low' ? 'text-green-500' : selectedPriority === 'medium' ? 'text-yellow-500' : 'text-red-500'}`} />
+                <FaCircle className={cn(`text-xs`, `text-priority-${selectedPriority}`)} />
                 {transl(`priority.${selectedPriority}` as const)}
               </span>
               {dropdownIcon}
             </button>
             {showDropdown && (
               <ul className={"absolute z-10 w-full bg-blue-100 mt-1 rounded-sm shadow-2xl"}>
-                {priority.map(({ id, name }) => (
+                {priority.map(({ id, name, color }) => (
                   <li key={id} className={"px-4 py-2 hover:bg-blue-200 cursor-pointer flex items-center gap-2"} onClick={() => { setSelectedPriority(name); setShowDropdown(false); }}>
-                    <FaCircle className={`text-xs text-priority-${name}`} />
+                    <FaCircle className={cn(`text-xs`, color)} />
                     {transl(`priority.${name}` as const)}
                   </li>
                 ))}
@@ -44,7 +46,7 @@ const NewTicketForm = () => {
             )}
             <input type="hidden" name="priority" value={selectedPriority} />
           </div>
-          <button className={"w-full bg-vcsblue text-white p-3 rounded hover:bg-blue-700 transition disabled:opacity-50"} type={"submit"}>
+          <button className={"w-full bg-vcsblue text-white p-3 rounded hover:bg-blue-900 transition disabled:opacity-50 cursor-pointer hover:shadow-sm shadow-lg"} type={"submit"}>
             {transl('submit.label')}
           </button>
         </form>
