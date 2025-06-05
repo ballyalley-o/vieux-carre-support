9656986342ca4ada063f618322b4f324e0e0c7f5
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-05 23:29:17 +1000

    [typings]: update global types for priority and logging levels

--- src/types/global.types.d.ts
+++ src/types/global.types.d.ts
@@ -1,5 +1,10 @@
+export {}
+
 declare global {
-    declare type PriorityType = {
-        LOW, MEDIUM, HIGH
-    }
+    declare type PriorityType    = 'LOW' |  'MEDIUM'| 'HIGH'
+    declare type PriorityKeyType = 'low' | 'medium' | 'high'
+
+    declare type AppLogLevelType = 'fatal' | 'error' | 'warning' | 'log' | 'info' | 'debug'
+    declare type AppResponseType = Promise<AppResponse>
+
 }
