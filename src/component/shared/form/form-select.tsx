327754903db1e73ce785e15abb53ba55780cc791
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-06 00:21:31 +1000

    [component]: enhance FormSelect component to accept selectedLabel prop for improved display

--- src/component/shared/form/form-select.tsx
+++ src/component/shared/form/form-select.tsx
@@ -1,4 +1,4 @@
-import { JSX, useState } from "react"
+import { JSX, ReactNode, useState } from "react"
 import { FaAngleUp, FaAngleDown } from "react-icons/fa"
 
 type FormSelectProps<T, V = string> = {
@@ -7,18 +7,18 @@ type FormSelectProps<T, V = string> = {
     options      : T[]
     value        : T | null;
     defaultValue : V
+    selectedLabel: ReactNode
     onChange     : (value: T) => void
     getValue     : (option: T) => V
     getColor    ?: (option: T) => string
     getIcon     ?: (option: T) => JSX.Element
     onToggle    ?: (open: boolean) => void
-    disabled    ?: boolean
     renderList   : (options: T[], onSelect: (value: T) => void) => JSX.Element
+    disabled    ?: boolean
 }
 
-function FormSelect<T, V>({ name, label, options, value, onChange, getValue, disabled = false, defaultValue, onToggle, renderList }: FormSelectProps<T, V>) {
+function FormSelect<T, V>({ name, label, options, value, onChange, getValue, disabled = false, defaultValue, onToggle, renderList, selectedLabel }: FormSelectProps<T, V>) {
     const [open, setOpen] = useState(false)
-    // const selectedLabel   = value ? getLabel(value) : defaultValue
     const selectedValue   = value ? getValue(value) : defaultValue
     const dropdownIcon    = open ?  <FaAngleUp/> : <FaAngleDown/>
 
@@ -30,15 +30,12 @@ function FormSelect<T, V>({ name, label, options, value, onChange, getValue, dis
       <div className={"relative"}>
        {label &&  <label className={'text-md py-4 mx-2 font-semibold'}>{label}</label>}
         <button type="button" className={'w-full border p-3 input-default flex justify-between items-center'} onClick={() => !disabled && toggleOpen(!open)} disabled={disabled}>
-          <span className={'flex items-center gap-2'}>{String(selectedValue)}</span>
+          <span className={'flex items-center gap-2'}>{selectedLabel}</span>
           {dropdownIcon}
         </button>
         {open && (
           <div className="absolute z-10 w-full mt-1">
-            {renderList(options, (option: T) => {
-              onChange(option)
-              toggleOpen(false)
-            })}
+            {renderList(options, (option: T) => { onChange(option); toggleOpen(false) })}
           </div>
         )}
         {name && <input type="hidden" name={name} value={selectedValue as PriorityType} />}
