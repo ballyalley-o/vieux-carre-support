fd94d02103c8698b6f58a1eff87d30689feb3649
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-05 23:51:54 +1000

    [refactor]: simplify HomePage component by removing commented logo section and improving link class formatting

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -8,15 +8,6 @@ const HomePage = () => {
   return (
     <main className={'flex flex-col text-center items-center justify-center min-h-screen px-4 gap-y-5'}>
       <div className={'flex flex-col text-center items-center justify-center w-[500px] h-auto px-4 gap-y-5'}>
-        {/* <div className={'flex flex-row items-center p-5'}>
-          <div className={'logo-home'}>
-            <Image src={ASSET_DIR.LOGO.svg} alt="logo" fill priority style={{ objectFit: 'contain' }} />
-          </div>
-          <span className={'ml-3 text-lg text-left'}>
-            <p className={'text-2xl font-bold'}>{GLOBAL.APP_NAME}</p>
-            <p className={'text-md'}>{GLOBAL.APP_DESCRIPTION}</p>
-          </span>
-        </div> */}
         <AppLogo />
         <div className={'flex flex-col md:flex-row gap-4 justify-center w-[350px] animate-slide opacity-0'}>
           <Link
@@ -26,9 +17,7 @@ const HomePage = () => {
           </Link>
           <Link
             href={PATH_DIR.TICKET.root}
-            className={
-              'bg-vcsblue-light w-full text-black px-4 py-1.5 rounded-xs shadow-sm hover:bg-blue-900 hover:shadow-lg hover:text-white transition text-sm'
-            }>
+            className={'bg-vcsblue-light w-full text-black px-4 py-1.5 rounded-xs shadow-sm hover:bg-blue-900 hover:shadow-lg hover:text-white transition text-sm'}>
             {transl('your_ticket.plural')}
           </Link>
         </div>
