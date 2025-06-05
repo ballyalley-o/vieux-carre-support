22508f44975439940442d57fc7313c02f573a9b7
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-05 23:53:05 +1000

    [db]: add Prisma client initialization for database management

---
+++ src/db/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from 'generated/prisma'
+import { KEY } from 'lib/constant'
+
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== KEY.PRODUCTION)
+globalForPrisma.prisma = prisma
