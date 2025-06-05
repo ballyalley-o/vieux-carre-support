4586392943052c6b91feeb961c7fae5be680bc4b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-05 23:50:03 +1000

    [refactor]: pass createTicket action to NewTicketForm for improved ticket creation

--- src/app/(root)/ticket/new/page.tsx
+++ src/app/(root)/ticket/new/page.tsx
@@ -1,9 +1,10 @@
 import NewTicketForm  from "app/(root)/ticket/new/new-ticket-form"
+import { createTicket } from "action/ticket.action"
 
 const NewTicketPage = () => {
   return (
     <div className={'min-h-screen flex items-center justify-center px-4'}>
-      <NewTicketForm />
+      <NewTicketForm action={createTicket} />
     </div>
   )
 }
