17ec0a7ef720ab5ccdae7d175aa692755cab3f8c
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-05 23:50:53 +1000

    [refactor]: update HomePage component to use AppLogo and clean up unused imports

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,6 @@
-import { GLOBAL } from 'vcs'
-import Image from 'next/image'
 import Link from 'next/link'
-import { ASSET_DIR, PATH_DIR } from 'vcs.dir'
+import { PATH_DIR } from 'vcs.dir'
+import { AppLogo } from 'component/shared/app'
 import { transl } from 'lib/utility'
 
 
@@ -9,7 +8,7 @@ const HomePage = () => {
   return (
     <main className={'flex flex-col text-center items-center justify-center min-h-screen px-4 gap-y-5'}>
       <div className={'flex flex-col text-center items-center justify-center w-[500px] h-auto px-4 gap-y-5'}>
-        <div className={'flex flex-row items-center p-5'}>
+        {/* <div className={'flex flex-row items-center p-5'}>
           <div className={'logo-home'}>
             <Image src={ASSET_DIR.LOGO.svg} alt="logo" fill priority style={{ objectFit: 'contain' }} />
           </div>
@@ -17,7 +16,8 @@ const HomePage = () => {
             <p className={'text-2xl font-bold'}>{GLOBAL.APP_NAME}</p>
             <p className={'text-md'}>{GLOBAL.APP_DESCRIPTION}</p>
           </span>
-        </div>
+        </div> */}
+        <AppLogo />
         <div className={'flex flex-col md:flex-row gap-4 justify-center w-[350px] animate-slide opacity-0'}>
           <Link
             href={PATH_DIR.TICKET.new}
@@ -26,7 +26,9 @@ const HomePage = () => {
           </Link>
           <Link
             href={PATH_DIR.TICKET.root}
-            className={'bg-vcsblue-light w-full text-black px-4 py-1.5 rounded-xs shadow-sm hover:bg-blue-900 hover:shadow-lg hover:text-white transition text-sm'}>
+            className={
+              'bg-vcsblue-light w-full text-black px-4 py-1.5 rounded-xs shadow-sm hover:bg-blue-900 hover:shadow-lg hover:text-white transition text-sm'
+            }>
             {transl('your_ticket.plural')}
           </Link>
         </div>
