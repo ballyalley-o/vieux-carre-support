427aebe64b27bd7f276765b6a4d476c1994ee09c
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-07 00:24:42 +1000

    [fix]: update base URL to use NEXT_PUBLIC_SERVER_URL environment variable

--- src/config/dir/path.ts
+++ src/config/dir/path.ts
@@ -1,10 +1,9 @@
-import { GLOBAL } from 'vcs'
 import { combine } from "lib/utility"
 
 const TICKET = 'ticket'
 const NEW    = 'new'
 
-export const _base = GLOBAL.SERVER_URL
+export const _base = process.env.NEXT_PUBLIC_SERVER_URL
 
 export const PATH_DIR = {
   TICKET: {
