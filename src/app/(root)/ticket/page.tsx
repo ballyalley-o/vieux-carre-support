1335b26f1d830816e6536beffedcaeeedf548aa1
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-07 00:22:37 +1000

    [feat]: add TicketsPage component to display all tickets

---
+++ src/app/(root)/ticket/page.tsx
@@ -0,0 +1,9 @@
+const TicketsPage = () => {
+    return (
+      <div className={'min-h-screen flex justify-center items-center align-middle m-auto'}>
+        <div className={''}> {'all tickets here'}</div>
+      </div>
+    )
+}
+
+export default TicketsPage
