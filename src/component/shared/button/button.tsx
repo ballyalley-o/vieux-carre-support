b2d82e92acc3711b9a7b91c355e2fa7f046c1a9b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-07 00:21:17 +1000

    enhance Button component to support link functionality and multiple variants

--- src/component/shared/button/button.tsx
+++ src/component/shared/button/button.tsx
@@ -2,19 +2,55 @@
 
 import { ReactNode } from 'react'
 import { useFormStatus } from 'react-dom'
+import Link from 'next/link'
 import { EllipsisLoader } from 'component/shared/loader'
+import { cn } from 'lib/utility'
 
-type ButtonProps = {
- type   ?: AppButtonType
- label   : ReactNode
+type ButtonVariantType = 'default' | 'primary' | 'secondary'
+type ButtonTypeProps = {
+  type   ?: AppButtonType
+  label   : ReactNode
+  variant : ButtonVariantType
+  link   ?: false
 }
-const Button = ({ label, type = 'submit' }: ButtonProps) => {
+type LinkButtonTypeProps = {
+  label  : ReactNode
+  variant: ButtonVariantType
+  link   : true
+  href   : string
+  type  ?: AppButtonType
+}
+
+type ButtonProps = ButtonTypeProps | LinkButtonTypeProps
+
+const Button = ({ label, type = 'submit', variant = 'default', link, ...rest }: ButtonProps) => {
   const { pending } = useFormStatus()
-    return (
-      <button className={'w-full bg-vcsblue text-white p-3 rounded hover:bg-blue-900 transition disabled:opacity-50 cursor-pointer hover:shadow-sm shadow-lg'} type={type} disabled={pending}>
+  const { href }    = rest as { href: string }
+
+  let _color;
+  switch (variant) {
+    case 'primary':
+      _color = 'bg-vcsblue text-white hover:bg-blue-900/80'
+      break
+    case 'secondary':
+      _color = 'bg-vcsblue-light hover:bg-vcsblue-light/90 text-black'
+      break
+    default:
+      _color = 'bg-vcsblue text-white hover:bg-blue-900'
+      break
+  }
+
+  if (link) {
+    return ( <Link href={href} className={cn('w-full p-3 rounded transition disabled:opacity-50 cursor-pointer hover:shadow-sm shadow-lg', _color)}>
         {pending ? <EllipsisLoader /> : label}
-      </button>
-    )
+      </Link>)
+  }
+
+  return (
+    <button className={cn('w-full p-3 rounded transition disabled:opacity-50 cursor-pointer hover:shadow-sm shadow-lg', _color )} type={type} disabled={pending}>
+      {pending ? <EllipsisLoader /> : label}
+    </button>
+  )
 }
 
 export default Button
