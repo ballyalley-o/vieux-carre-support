a050c31c672f7a6bc5f366d441d198bbcd21942f
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-07 18:59:44 +1000

    [refactor]: replace hardcoded 404 error messages with localized translations

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image'
 import { Button } from 'component/shared/button'
 import { ASSET_DIR } from 'vcs.dir'
-import { CODE, HTTP_RESPONSE } from 'lib/constant'
+import { CODE } from 'lib/constant'
 import { transl } from 'lib/utility'
 
 const NotFoundPage = () => {
@@ -13,8 +13,8 @@ const NotFoundPage = () => {
       <Image src={ASSET_DIR.LOGO.svg} width={48} height={48} alt="logo" priority />
       <div className="p-6 w-1/3 rounded-sm text-center">
         <h4 className="text-lg mt-2">{CODE.NOT_FOUND}</h4>
-        <h1 className="text-3xl font-bold mb-4">{HTTP_RESPONSE[404].title}</h1>
-        <h6 className="mt-8">{HTTP_RESPONSE[404].description}</h6>
+        <h1 className="text-3xl font-bold mb-4">{transl('http_response.404.label')}</h1>
+        <h6 className="mt-8">{transl('http_response.404.description')}</h6>
         <Button variant={'outline'} label={transl('go_back')} onClick={handleNavigateHome} className={'my-5'} />
       </div>
     </div>
