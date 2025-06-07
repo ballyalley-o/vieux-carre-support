59f65935e8322c7a3da65fb6e594ccfba909834a
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-07 18:13:59 +1000

    add NotFoundPage component with navigation and error messaging

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Image from 'next/image'
+import { Button } from 'component/shared/button'
+import { ASSET_DIR } from 'vcs.dir'
+import { CODE, HTTP_RESPONSE } from 'lib/constant'
+import { transl } from 'lib/utility'
+
+const NotFoundPage = () => {
+  const handleNavigateHome = () => (window.location.href = '/')
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen shadow-none">
+      <Image src={ASSET_DIR.LOGO.svg} width={48} height={48} alt="logo" priority />
+      <div className="p-6 w-1/3 rounded-sm text-center">
+        <h4 className="text-lg mt-2">{CODE.NOT_FOUND}</h4>
+        <h1 className="text-3xl font-bold mb-4">{HTTP_RESPONSE[404].title}</h1>
+        <h6 className="mt-8">{HTTP_RESPONSE[404].description}</h6>
+        <Button variant={'outline'} label={transl('go_back')} onClick={handleNavigateHome} className={'my-5'} />
+      </div>
+    </div>
+  )
+}
+
+export default NotFoundPage
