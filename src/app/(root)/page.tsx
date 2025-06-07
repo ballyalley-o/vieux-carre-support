9ba44cfbef634ffe1862242a4650d1bff6a91f8d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-07 18:13:51 +1000

    [enhance-propagation]: set Button components to full width on HomePage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -10,8 +10,8 @@ const HomePage = () => {
       <div className={'flex flex-col text-center items-center justify-center w-[500px] h-auto px-4 gap-y-5'}>
         <AppLogo />
         <div className={'flex flex-col md:flex-row gap-4 justify-center w-[350px] animate-slide opacity-0'}>
-          <Button link href={PATH_DIR.TICKET.new} label={transl('create_ticket.label')} variant={'default'} />
-          <Button link href={PATH_DIR.TICKET.root} label={transl('your_ticket.plural')} variant={'secondary'} />
+          <Button link href={PATH_DIR.TICKET.new} label={transl('create_ticket.label')} variant={'default'} fullWidth />
+          <Button link href={PATH_DIR.TICKET.root} label={transl('your_ticket.plural')} variant={'secondary'} fullWidth />
         </div>
       </div>
     </main>
