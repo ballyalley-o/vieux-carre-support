a9edc3486c826cff71eeff53a867aa56b898b5a3
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-08 01:18:59 +1000

    add optional header prop to Card component

--- src/component/shared/card/card.tsx
+++ src/component/shared/card/card.tsx
@@ -4,22 +4,20 @@ import { cn } from 'lib/utility'
 interface CardProps {
   title     : string
   icon     ?: ReactNode
+  header   ?: string
   children ?: ReactNode
   className?: string
 }
 
-const Card = ({ title, icon, children, className }: CardProps) => {
+const Card = ({ title, icon, header, children, className }: CardProps) => {
   return (
     <article className={cn('flex justify-between items-center rounded-sm shadow-sm border border-gray-200 p-6 bg-blue-50', className)}>
       <div>
-        <h2 className="text-xl font-semibold text-blue-600">{title}</h2>
+        {header && <h4 className={"text-sm font-semibold text-gray-700"}>{header}</h4>}
+        <h2 className={"text-xl font-semibold text-blue-600"}>{title}</h2>
       </div>
-      <div className="text-right space-y-2">
-        {icon && (
-          <div className="text-sm text-gray-500 flex items-center gap-2">
-            {icon}
-          </div>
-        )}
+      <div className={"text-right space-y-2"}>
+        {icon && <div className={"text-sm text-gray-500 flex items-center gap-2"}>{icon}</div>}
         {children}
       </div>
     </article>
