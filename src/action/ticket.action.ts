af080ed69624b37b1c01c6a73818f73a47f513c9
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-07 18:12:53 +1000

    [action]: add getTickets function to fetch tickets with filtering and pagination

--- src/action/ticket.action.ts
+++ src/action/ticket.action.ts
@@ -1,11 +1,12 @@
 'use server'
 
+import { GLOBAL } from 'vcs'
 import { prisma } from 'vcs.db'
 import { PATH_DIR } from 'vcs.dir'
 import { revalidatePath } from 'next/cache'
-import { TicketPriority } from 'generated/prisma'
+import { Prisma, TicketPriority } from '@prisma/client'
 import { SystemLogger } from "lib/utility/app-logger"
-import { CODE } from "lib/constant"
+import { CODE, KEY } from "lib/constant"
 import { transl } from "lib/utility"
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -31,9 +32,40 @@ export async function createTicket(prevState: AppResponse, formData: FormData): 
 
         revalidatePath(PATH_DIR.TICKET.root)
         return SystemLogger.response(true, transl('success.created'), CODE.OK, { subject, description, priority })
-    } catch (error: AppError) {
-        const _errorMessage = transl('error.create_ticket', { error: error.message })
+    } catch (error: unknown) {
+        const _errorMessage = transl('error.create_ticket', { error: (error as AppError).message })
         SystemLogger.sentryLogEvent(_errorMessage, MODULE, { formData: Object.fromEntries(formData.entries()) }, 'error', error)
         return SystemLogger.response(false, _errorMessage, CODE.BAD_REQUEST, {})
+    }
+}
+
+export async function getTickets({ query, limit = GLOBAL.LIMIT.PAGE_SIZE, page, category, sort }:AppTicketsAction<number>) {
+    try {
+        const queryFilter: Prisma.TicketWhereInput = query && query       !== KEY.ALL ? ({ subject: { contains: query, mode: 'insensitive' } }) : {}
+        const categoryFilter                       = category && category !== KEY.ALL ? { category: { equals: category } } : {}
+        const tickets                              = await prisma.ticket.findMany({
+          where: { ...queryFilter, ...categoryFilter },
+          orderBy:
+            sort === 'oldest'
+              ? { createdAt: 'asc' }
+              : sort === 'newest'
+                ? { createdAt: 'desc' }
+                : sort === 'priority'
+                  ? { priority: 'desc' }
+                  : { createdAt: 'desc' },
+          skip: (page - 1) * limit,
+          take: limit
+        })
+        const count    = await prisma.ticket.count({ where: { ...queryFilter } })
+
+        const _message = transl('success.fetched_default', { value: MODULE, count:tickets.length })
+        SystemLogger.sentryLogEvent(_message, MODULE, { count: tickets.length }, 'info')
+
+        const summary = { data: tickets, totalPages: Math.ceil(count/limit) }
+        return summary
+    } catch (error) {
+        const _errorMessage = transl(`error.failed_fetch_default`, { value: MODULE })
+        SystemLogger.sentryLogEvent(_errorMessage, MODULE, {}, 'error', error)
+        return { data: [], totalPages: 0 }
     }
 }
