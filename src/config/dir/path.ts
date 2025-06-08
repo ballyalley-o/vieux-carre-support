ae013dccbca8dff793d18d4865771ae9c76738c7
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-08 17:08:41 +1000

    [config-dir]: add HOME path constant to PATH_DIR configuration

--- src/config/dir/path.ts
+++ src/config/dir/path.ts
@@ -1,11 +1,13 @@
 import { combine, formatTicketId } from "lib/utility"
 
+const HOME   = '/'
 const TICKET = 'ticket'
 const NEW    = 'new'
 
 export const _base = process.env.NEXT_PUBLIC_SERVER_URL
 
 export const PATH_DIR = {
+  HOME: HOME,
   TICKET: {
     root: combine(TICKET),
     new : combine(TICKET, NEW),
