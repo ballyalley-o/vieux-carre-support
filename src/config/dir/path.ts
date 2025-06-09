d2e939db83c4a87aefb2751fdd29c87b4cebdd34
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-09 23:50:10 +1000

    [config-dir]:  add authentication paths for sign-in, sign-up, and sign-out

--- src/config/dir/path.ts
+++ src/config/dir/path.ts
@@ -1,16 +1,24 @@
 import { combine, formatTicketId } from "lib/utility"
 
-const HOME   = '/'
-const TICKET = 'ticket'
-const NEW    = 'new'
+const HOME    = '/'
+const TICKET  = 'ticket'
+const NEW     = 'new'
+const SIGN_IN = 'sign-in'
+const SIGN_UP = 'sign-up'
+const SIGN_OUT = 'sign-out'
 
 export const _base = process.env.NEXT_PUBLIC_SERVER_URL
 
 export const PATH_DIR = {
-  HOME: HOME,
+  HOME  : HOME,
   TICKET: {
     root: combine(TICKET),
     new : combine(TICKET, NEW),
     id  : (id: number) => combine(TICKET, formatTicketId(id))
+  },
+  AUTH: {
+    sign_in : combine(SIGN_IN),
+    sign_up : combine(SIGN_UP),
+    sign_out: combine(SIGN_OUT)
   }
 }
