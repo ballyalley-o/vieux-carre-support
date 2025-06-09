09d698e7d6cb7eddd4784cbe55ba24c3d241f42d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-10 01:04:57 +1000

    [feat-refactor] refactor: use a single function to handle input events

---
+++ src/component/shared/form/form-password-input.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useState } from 'react'
+import { FaEyeSlash, FaEye } from 'react-icons/fa'
+import { transl } from 'lib/utility'
+
+interface FormPasswordInputProps {
+    name               : string
+    onClick           ?: () => void
+    setConfirmPassword?: React.Dispatch<React.SetStateAction<string>>
+}
+
+const FormPasswordInput = ({ name, setConfirmPassword }: FormPasswordInputProps) => {
+    const [viewPassword, setViewPassword] = useState(false)
+
+    const togglePasswordVisibility = () => setViewPassword((prev) => !prev)
+    const toggleShowPasswordIcon   = (_bool: boolean) => _bool ? <FaEyeSlash className = {'text-gray-400'} /> : <FaEye className = {'text-gray-400'} />
+
+
+    return (
+        <div className={'relative'}>
+            <input className={'w-full input-default pr-10'} type={viewPassword ? 'text': "password"} name={name} autoComplete={'new-password'} placeholder={transl('form.confirm_password.placeholder')} onChange={(e) => setConfirmPassword?.(e.target.value)} required />
+            <button type={'button'} className={'absolute inset-y-0 right-2 flex items-center px-2  cursor-pointer text-xl'} onClick={togglePasswordVisibility}  aria-label={viewPassword ? transl('hide_password.label') : transl('show_password.label')}>{toggleShowPasswordIcon(viewPassword)}</button>
+        </div>
+      )
+}
+
+export default FormPasswordInput
--- src/component/shared/form/index.ts
+++ src/component/shared/form/index.ts
@@ -1,1 +1,2 @@
 export { default as FormSelect } from './form-select'
+export { default as FormPasswordInput } from './form-password-input'
