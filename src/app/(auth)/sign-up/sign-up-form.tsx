c1b7a7b671be568655940bf00fa5fb9fd9eb399d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-10 01:04:28 +1000

    refactor/cleanup

--- src/app/(auth)/sign-up/sign-up-form.tsx
+++ src/app/(auth)/sign-up/sign-up-form.tsx
@@ -5,8 +5,8 @@ import { PATH_DIR } from "vcs.dir"
 import { useRouter } from 'next/navigation'
 import { signUp } from "action/auth.action"
 import { toast } from "sonner"
-import { FaEye, FaEyeSlash } from 'react-icons/fa'
 import { Button } from 'component/shared/button'
+import { FormPasswordInput } from 'component/shared/form'
 import { RESPONSE } from "lib/constant"
 import { cn, transl } from 'lib/utility'
 
@@ -15,8 +15,6 @@ interface SignUpFormProps {
 }
 
 const SignUpForm = ({ action }: SignUpFormProps) => {
-    const [viewPassword, setViewPassword]               = useState(false)
-    const [viewConfirmPassword, setViewConfirmPassword] = useState(false)
     const [confirmPassword, setConfirmPassowrd]         = useState('')
     const [state, formAction]                           = useActionState(action, RESPONSE.DEFAULT)
     const router                                        = useRouter()
@@ -32,9 +30,6 @@ const SignUpForm = ({ action }: SignUpFormProps) => {
     }, [state, router])
 
     const renderStateMessage              = state.message && !state.success && <p className={'text-red-500 mb-4 text-center'}>{state.message}</p>
-    const togglePasswordVisibility        = () => setViewPassword(prev => !prev)
-    const toggleConfirmPasswordVisibility = () => setViewConfirmPassword((prev) => !prev)
-    const toggleShowPasswordIcon          = (_bool: boolean) => _bool ? <FaEyeSlash className={'text-gray-400'} /> : <FaEye className={'text-gray-400'} />
 
     const handleFormSubmit = (e:React.FormEvent<HTMLFormElement>) => {
       const form     = e.currentTarget
@@ -52,14 +47,8 @@ const SignUpForm = ({ action }: SignUpFormProps) => {
           {renderStateMessage}
           <input className={'w-full input-default'} type="text" name={'name'} autoCapitalize={'on'} placeholder={transl('form.name.placeholder')} required />
           <input className={'w-full input-default'} type="email" name={'email'} autoComplete={'email'} placeholder={transl('form.email.placeholder')} required />
-          <div className={'relative'}>
-            <input className={'w-full input-default 10 pr-10'} type={viewPassword ? 'text': "password"} name={'password'} autoComplete={'new-password'} placeholder={transl('form.password.placeholder')} required />
-            <button type={'button'} className={'absolute inset-y-0 right-2 flex items-center px-2 cursor-pointer text-xl'} onClick={togglePasswordVisibility}  aria-label={viewPassword ? transl('show_password.label') : transl('hide_password.label')}>{toggleShowPasswordIcon(viewPassword)}</button>
-          </div>
-          <div className={'relative'}>
-            <input className={'w-full input-default pr-10'} type={viewConfirmPassword ? 'text': "password"} name={'confirm-password'} autoComplete={'new-password'} placeholder={transl('form.confirm_password.placeholder')} onChange={(e) => setConfirmPassowrd(e.target.value)}  required />
-            <button type={'button'} className={'absolute inset-y-0 right-2 flex items-center px-2  cursor-pointer text-xl'} onClick={toggleConfirmPasswordVisibility}  aria-label={viewPassword ? transl('hide_password.label') : transl('show_password.label')}>{toggleShowPasswordIcon(viewConfirmPassword)}</button>
-          </div>
+          <FormPasswordInput name={'password'} />
+          <FormPasswordInput name={'confirmPassword'} setConfirmPassword={setConfirmPassowrd}/>
           <Button variant={'default'} label={transl('sign_up.label')} fullWidth />
         </form>
       </div>
