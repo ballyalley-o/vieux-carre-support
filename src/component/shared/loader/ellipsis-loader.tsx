f5d77f57f1f2047786716463dc2c83f8e7699fdd
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-10 22:38:51 +1000

    [feat]: added label in the loader

--- src/component/shared/loader/ellipsis-loader.tsx
+++ src/component/shared/loader/ellipsis-loader.tsx
@@ -5,11 +5,13 @@ interface EllipsisLoaderProps {
   className?: string
   dotSize  ?: string
   dotColor ?: string
+  label    ?: string
 }
 
-const EllipsisLoader: FC<EllipsisLoaderProps> = ({ className, dotSize = 'text-xl', dotColor = 'text-white', ...props }) => {
+const EllipsisLoader: FC<EllipsisLoaderProps> = ({ className, dotSize = 'text-lg', dotColor = 'text-white', label, ...props }) => {
   return (
-    <div className={cn('flex text-center justify-center gap-1', className)} {...props}>
+    <div className={cn('flex items-center text-center justify-center gap-1', className)} {...props}>
+      {label  && <span className={cn('animate-pulse', dotSize, dotColor)}>{label}</span>}
       <span className={cn('dot-animation', dotSize, dotColor)}>{'.'}</span>
       <span className={cn('dot-animation delay-200', dotSize, dotColor)}>{'.'}</span>
       <span className={cn('dot-animation delay-400', dotSize, dotColor)}>{'.'}</span>
