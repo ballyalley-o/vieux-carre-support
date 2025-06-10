f9e1c8ff8321a21e40f5fbb7f6842df08f53bada
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-10 22:36:13 +1000

    applied redirect options in authentications

--- src/app/(auth)/sign-up/sign-up-form.tsx
+++ src/app/(auth)/sign-up/sign-up-form.tsx
@@ -5,6 +5,7 @@ import { PATH_DIR } from "vcs.dir"
 import { useRouter } from 'next/navigation'
 import { signUp } from "action/auth.action"
 import { toast } from "sonner"
+import { AppAuthRedir } from 'component/shared/app'
 import { Button } from 'component/shared/button'
 import { FormPasswordInput } from 'component/shared/form'
 import { RESPONSE } from "lib/constant"
@@ -41,15 +42,16 @@ const SignUpForm = ({ action }: SignUpFormProps) => {
     }
 
     return (
-      <div className={cn('w-auto md:w-[500px] max-w-lg shadow-md rounded-sm p-8 transition-colors duration-300 ease-in-out ','bg-blue-50')}>
+      <div className={cn('w-auto md:w-[500px] max-w-lg rounded-sm p-8 transition-colors duration-300 ease-in-out ')}>
         <h1 className={'text-2xl font-bold mb-6 text-center text-black'}>{transl('sign_up.label')}</h1>
         <form action={formAction} onSubmit={handleFormSubmit} className={'space-y-4 text-gray-700'}>
           {renderStateMessage}
           <input className={'w-full input-default'} type="text" name={'name'} autoCapitalize={'on'} placeholder={transl('form.name.placeholder')} required />
           <input className={'w-full input-default'} type="email" name={'email'} autoComplete={'email'} placeholder={transl('form.email.placeholder')} required />
           <FormPasswordInput name={'password'} />
-          <FormPasswordInput name={'confirmPassword'} setConfirmPassword={setConfirmPassowrd}/>
-          <Button variant={'default'} label={transl('sign_up.label')} fullWidth />
+          <FormPasswordInput name={'confirmPassword'} setConfirmPassword={setConfirmPassowrd} />
+          <Button variant={'default'} label={transl('sign_up.label')} loadingLabel={transl('sign_up.loading')} fullWidth />
+          <AppAuthRedir type={'sign-up'}/>
         </form>
       </div>
     )
