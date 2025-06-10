7a7cc9ec89218efbacb4a9aaab944f6e2e9a197c
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-10 22:34:46 +1000

    applied consistency

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,7 +4,7 @@ import { signIn } from 'action/auth.action'
 const SignInPage = () => {
 
     return (
-      <div className={'min-h-screen flex items-center justify-center px-4'}>
+      <div className={'min-h-screen flex items-start md:mt-20 justify-center px-4'}>
         <SignInForm action={signIn} />
       </div>
     )
