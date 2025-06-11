7624068e2ca5725c27d71a86e97a9c156d7cfec7
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-11 11:49:15 +1000

    [refactor]: for clearer code

---
+++ src/component/module/ticket/ticket-item.tsx
@@ -0,0 +1,22 @@
+import { PATH_DIR } from "vcs.dir"
+import Link from "next/link"
+import type { Ticket } from "@prisma/client"
+import { FaCircle } from 'react-icons/fa'
+import { Card } from "component/shared/card"
+import { formatTicketId, formatText, transl, cn } from "lib/utility"
+
+interface TicketItemProps {
+    ticket: Ticket
+}
+
+const TicketItem = ({ ticket }: TicketItemProps) => {
+    return (
+         <Card  header={formatTicketId(ticket.id)} title={ticket.subject} icon={<div className={'align-middle items-center-safe flex gap-2'}><FaCircle className={cn(`text-priority-${formatText(ticket.priority, 'lowercase')}`)}/> <span>{formatText(ticket.priority, 'capitalize')}</span></div>}>
+            <Link href={PATH_DIR.TICKET.id(ticket.id)} className={'inline-block mt-2 bg-blue-600 text-white text-sm px-3 py-1 rounded-sm hover:bg-blue-700 transition text-center'}>
+                {transl('view_ticket.label')}
+            </Link>
+        </Card>
+     );
+}
+
+export default TicketItem;
