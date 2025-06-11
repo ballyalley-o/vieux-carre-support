813d5471d23c43330f1e5cb5c8efa9269432802d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-11 23:15:34 +1000

    [refactor-feat]: added status selector for view ticket

--- src/component/module/ticket/index.ts
+++ src/component/module/ticket/index.ts
@@ -1,3 +1,4 @@
 export { default as TicketItem } from './ticket-item'
 export { default as TicketCardTitle } from './ticket-card-title'
 export { default as TicketCloseButton } from './ticket-close-button'
+export { default as TicketStatusSelector } from './ticket-status-selector'
