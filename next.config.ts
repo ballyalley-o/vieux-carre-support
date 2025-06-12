b55f3ff4ad57900f08bf219a601bb3f3779bec87
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-12 15:58:24 +1000

    [fix]:  basepath for proxy host

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,19 @@
 import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
+const isProxyHost = process.env.PROXY_HOST === 'vieux-carre.vercel.app'
+
 const nextConfig: NextConfig = {
-  basePath   : '/support',
-  assetPrefix: '/support'
+  basePath   : isProxyHost ?  '/support': '',
+  assetPrefix: isProxyHost ?  '/support': '',
 }
 
 export default withSentryConfig(nextConfig, {
-org                    : "goodekit",
-project                : "vieux-carre-support",
-silent                 : !process.env.CI,
-widenClientFileUpload  : true,
-tunnelRoute            : "/monitoring",
-disableLogger          : true,
-automaticVercelMonitors: true,
+  org                    : "goodekit",
+  project                : "vieux-carre-support",
+  silent                 : !process.env.CI,
+  widenClientFileUpload  : true,
+  tunnelRoute            : "/monitoring",
+  disableLogger          : true,
+  automaticVercelMonitors: true,
 })
