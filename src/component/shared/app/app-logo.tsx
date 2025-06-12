7287ee109e6de0d24a2a6cb82bdc482a5e6596de
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-12 15:29:17 +1000

    [interactivity]: font sizes reduce for mobile devices

--- src/component/shared/app/app-logo.tsx
+++ src/component/shared/app/app-logo.tsx
@@ -6,11 +6,11 @@ const AppLogo = () => {
     return (
         <div className={'flex flex-row items-center p-5'}>
             <div className={'logo-home'}>
-                <Image src={ASSET_DIR.LOGO.svg} alt="logo" fill priority objectFit={'contain'} />
+                <Image src={ASSET_DIR.LOGO.svg} alt="logo" fill priority style={{ objectFit: 'contain' }} />
             </div>
             <span className={'ml-3 text-lg text-left'}>
-                <p className={'text-2xl font-bold'}>{GLOBAL.APP_NAME}</p>
-                <p className={'text-md'}>{GLOBAL.APP_DESCRIPTION}</p>
+                <p className={'text-xl md:text-2xl font-bold'}>{GLOBAL.APP_NAME}</p>
+                <p className={'text-sm md:text-md'}>{GLOBAL.APP_DESCRIPTION}</p>
             </span>
         </div>
     );
