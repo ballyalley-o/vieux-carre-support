3c795347a85b7fa916d486967ddc7f011a8ea721
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-12 15:29:33 +1000

    [interactivity]: font sizes reduce for mobile devices

--- src/component/shared/button/sign-out-button.tsx
+++ src/component/shared/button/sign-out-button.tsx
@@ -5,6 +5,7 @@ import { PATH_DIR } from 'vcs.dir'
 import { useRouter } from "next/navigation"
 import { signOut } from "action/auth.action"
 import { toast } from "sonner"
+import { PiSignOutBold } from 'react-icons/pi'
 import { Button } from 'component/shared/button'
 import { RESPONSE } from "lib/constant"
 import { transl } from "lib/utility"
@@ -22,9 +23,19 @@ const SignOutButton = () => {
         }
     }, [state, router])
     return (
-        <form action={formAction}>
-            <Button type={'submit'} variant={'desctructive'} className={'text-white transition'} label={transl('sign_out.label')} />
-        </form>
+      <form action={formAction}>
+        <Button
+          type={'submit'}
+          variant={'desctructive'}
+          className={'text-white transition'}
+          label={
+            <div className={'flex items-center justify-center'}>
+              <PiSignOutBold />
+              <p className={'text-sm md:text-md hidden md:block'}>{transl('sign_out.label')}</p>
+            </div>
+          }
+        />
+      </form>
     )
 }
 
