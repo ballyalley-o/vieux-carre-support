e1278ee7946239d321134f129d504b3ccf7b5cc1
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-12 15:28:49 +1000

    [interactivity]: font sizes reduce for mobile devices

--- src/component/shared/app/app-logo-navbar.tsx
+++ src/component/shared/app/app-logo-navbar.tsx
@@ -2,17 +2,19 @@ import { GLOBAL } from 'vcs'
 import { ASSET_DIR, PATH_DIR } from 'vcs.dir'
 import Link from 'next/link'
 import Image from 'next/image'
+import { transl } from 'lib/utility'
 
 const AppLogoNavbar = () => {
     return (
       <div className={'flex flex-row items-center'}>
         <div className={'logo-navbar'}>
           <Link href={PATH_DIR.HOME}>
-            <Image src={ASSET_DIR.LOGO.svg} alt={"logo"} fill priority objectFit={'contain'} />
+            <Image src={ASSET_DIR.LOGO.svg} alt={'logo'} fill priority style={{ objectFit: 'contain' }} />
           </Link>
         </div>
         <span className={'ml-3 text-lg text-left'}>
-          <p className={'text-md font-bold'}>{GLOBAL.APP_NAME}</p>
+          <p className={'text-sm md:text-md font-bold hidden md:block'}>{GLOBAL.APP_NAME}</p>
+          <p className={'text-sm md:text-md font-bold md:hidden'}>{transl('support.label')}</p>
         </span>
       </div>
     )
