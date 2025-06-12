193cab5f4dc880b3bde33d32c96304fedcf266ef
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-12 15:59:42 +1000

    [config]: proxy host variable in globals

--- src/config/global.ts
+++ src/config/global.ts
@@ -39,7 +39,8 @@ const envSchema = z.object({
   HASH_MEMORY_COST           : z.string().min(1, _msg('HASH_MEMORY_COST')),
   HASH_TIME_COST             : z.string().min(1, _msg('HASH_TIME_COST')),
   HASH_PARALLELISM           : z.string().min(1, _msg('HASH_PARALLELISM')),
-  ADMIN_EMAILS               : z.string().min(1, _msg('ADMIN_EMAILS'))
+  ADMIN_EMAILS               : z.string().min(1, _msg('ADMIN_EMAILS')),
+  PROXY_HOST                 : z.string().min(1, _msg('PROXY_HOST'))
 })
 
 let parsedEnv: z.infer<typeof envSchema> | null = null
@@ -73,7 +74,8 @@ function getEnv() {
       HASH_MEMORY_COST           : '19456',
       HASH_TIME_COST             : '2',
       HASH_PARALLELISM           : '1',
-      ADMIN_EMAILS               : ''
+      ADMIN_EMAILS               : '',
+      PROXY_HOST                 : ''
     }
   }
 
@@ -122,6 +124,9 @@ export const GLOBAL = {
   },
   get ADMIN_EMAILS() {
     return getEnv().ADMIN_EMAILS.split(';')
+  },
+  get PROXY_HOST() {
+    return getEnv().PROXY_HOST
   },
   LIMIT: {
     PAGE_SIZE: 7
