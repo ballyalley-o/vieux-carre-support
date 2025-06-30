1201dbd4f8444ffcc3e5434919a953386ecd976a
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-30 18:24:19 +1000

    #8 [refactor]: update sign-in and sign-up forms to use react-hook-form and improve submission handling; Fixes #8

--- src/app/(auth)/sign-in/sign-in-form.tsx
+++ src/app/(auth)/sign-in/sign-in-form.tsx
@@ -1,49 +1,57 @@
 'use client'
 
-import { useActionState, useEffect } from 'react'
 import { PATH_DIR } from "vcs.dir"
 import { useRouter, useSearchParams } from 'next/navigation'
+import { SubmitHandler, FormProvider, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { signUp } from "action/auth.action"
 import { toast } from "sonner"
 import { AppAuthRedir } from 'component/shared/app'
 import { Button } from 'component/shared/button'
-import { FormPasswordInput } from 'component/shared/form'
-import { RESPONSE } from "lib/constant"
+import { RHFFormField, RHFPasswordField } from 'component/shared/rhf'
 import { cn, transl } from 'lib/utility'
+import { SignInSchema } from 'lib/schema'
+import { delay } from 'lib/utility'
 
-interface SignUpFormProps {
+interface SignInFormProps {
   action: typeof signUp
 }
 
-const SignUpForm = ({ action }: SignUpFormProps) => {
-    const [state, formAction] = useActionState(action, RESPONSE.DEFAULT)
-    const router              = useRouter()
-    const searchParams        = useSearchParams()
-    const callbackUrl         = searchParams.get('callbackUrl') || PATH_DIR.HOME
+const SignInForm = ({ action }: SignInFormProps) => {
+    const router       = useRouter()
+    const searchParams = useSearchParams()
+    const callbackUrl  = searchParams.get('callbackUrl') || PATH_DIR.HOME
+    const form         = useForm<SignIn>({ resolver: zodResolver(SignInSchema), defaultValues: { email: '', password: '' } })
 
-    useEffect(() => {
-      if (state.success) {
-        toast.success(transl('success.signed_in'))
-        router.push(callbackUrl)
-        router.refresh()
-      }
-    }, [state.success, router, callbackUrl])
+    const { control, register, formState: { isSubmitting }, handleSubmit } = form
 
-    const renderStateMessage       = state.message && !state.success && <p className = {'text-red-500 mb-4 text-center'}>{state.message}</p>
+   const onSubmit: SubmitHandler<SignIn> = async (data) => {
+    try {
+      const response = await action(data)
+      await delay(500)
+      toast.success(response.message)
+      router.push(callbackUrl)
+      router.refresh()
+    } catch (error) {
+      console.log('error: ', error)
+      toast.error((error as AppError)?.message)
+    }
+   }
 
     return (
       <div className={cn('w-auto md:w-[500px] max-w-lg rounded-sm p-8 transition-colors duration-300 ease-in-out ')}>
         <h1 className={'text-2xl font-bold mb-6 text-center text-black'}>{transl('sign_in.label')}</h1>
-        <form action={formAction} className={'space-y-4 text-gray-700'}>
-          {renderStateMessage}
-          <input className={'w-full input-default'} type="email" name={'email'} autoComplete={'email'} placeholder={transl('form.email.placeholder')} required />
-          <FormPasswordInput name={'password'} />
-          <Button variant={'default'} label={transl('sign_in.label')} loadingLabel={transl('sign_in.loading')} fullWidth />
-          <AppAuthRedir type={'sign-in'}/>
-        {/*TODO:  add google-auth or integrate with vieux-carre authentication */}
-        </form>
+        <FormProvider {...form}>
+          <form method={'POST'} onSubmit={handleSubmit(onSubmit)} className={'space-y-4 text-gray-700'}>
+            <RHFFormField control={control} name={'email'} formKey={'email'} disabled={isSubmitting} withWrapper />
+            <RHFPasswordField control={control} register={register} name={'password'} formKey={'password'} disabled={isSubmitting} withWrapper />
+            <Button variant={'default'} label={transl('sign_in.label')} loadingLabel={transl('sign_in.loading')} loading={isSubmitting} fullWidth />
+            <AppAuthRedir type={'sign-in'} />
+            {/*TODO:  add google-auth or integrate with vieux-carre authentication */}
+          </form>
+        </FormProvider>
       </div>
     )
 }
 
-export default SignUpForm
+export default SignInForm
