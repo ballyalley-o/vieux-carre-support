345e0fc13034e232341501761b11329b9d7940a2
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-05 01:11:00 +1000

    [fix]

--- src/app/(root)/ticket/new/page.tsx
+++ src/app/(root)/ticket/new/page.tsx
@@ -1,7 +1,22 @@
+import { headers } from "next/headers"
 import { createTicket } from "action/ticket.action"
 import NewTicketForm  from "app/(root)/ticket/new/new-ticket-form"
 
+import { auth } from "vieux-carre.authenticate"
+import { PATH_DIR } from "vcs.dir"
+import { redirect } from "next/navigation"
+
 const NewTicketPage = async () => {
+  const user = await auth()
+  if (!user) {
+    // Construct the absolute callbackUrl using the current request URL
+    const headersList = await headers()
+    const protocol    = headersList.get("x-forwarded-proto") || "https"
+    const host        = headersList.get("host")
+    const url         = `${protocol}://${host}/ticket/new`
+    const callbackUrl = encodeURIComponent(url)
+    redirect(`${PATH_DIR.AUTH.sign_in}?callbackUrl=${callbackUrl}`)
+  }
   return (
     <div className={'min-h-screen flex items-center justify-center px-4'}>
       <NewTicketForm action={createTicket} />
