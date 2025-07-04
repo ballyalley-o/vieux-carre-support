40c2f080e0cf26de81b43c8aadf00606f0da076a
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-04 20:33:52 +1000

    #17 [enhance]: enhanced the  navbar layout and changed the href to direct user to main platform

--- src/component/shared/app/app-logo-navbar.tsx
+++ src/component/shared/app/app-logo-navbar.tsx
@@ -1,21 +1,15 @@
-import { GLOBAL } from 'vcs'
 import { ASSET_DIR, PATH_DIR } from 'vcs.dir'
 import Link from 'next/link'
 import Image from 'next/image'
-import { transl } from 'lib/utility'
 
 const AppLogoNavbar = () => {
     return (
       <div className={'flex flex-row items-center'}>
         <div className={'logo-navbar'}>
-          <Link href={PATH_DIR.HOME}>
+          <Link href={PATH_DIR.MAIN}>
             <Image src={ASSET_DIR.LOGO.svg} alt={'logo'} fill priority style={{ objectFit: 'contain' }} />
           </Link>
         </div>
-        <span className={'ml-3 text-lg text-left'}>
-          <p className={'text-sm md:text-md font-bold hidden md:block'}>{GLOBAL.APP_NAME}</p>
-          <p className={'text-sm md:text-md font-bold md:hidden'}>{transl('support.label')}</p>
-        </span>
       </div>
     )
 }
