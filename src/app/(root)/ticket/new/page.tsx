31440c0354d0543addd08a53bf4ebe473e2b1a77
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-05 01:31:15 +1000

    implemented the getter of absoluteurl

--- src/app/(root)/ticket/new/page.tsx
+++ src/app/(root)/ticket/new/page.tsx
@@ -1,19 +1,14 @@
-import { headers } from "next/headers"
-import { createTicket } from "action/ticket.action"
-import NewTicketForm  from "app/(root)/ticket/new/new-ticket-form"
-
 import { auth } from "vieux-carre.authenticate"
 import { PATH_DIR } from "vcs.dir"
+import { createTicket } from "action/ticket.action"
 import { redirect } from "next/navigation"
+import { getAbsoluteUrl } from "lib/next"
+import NewTicketForm  from "app/(root)/ticket/new/new-ticket-form"
 
 const NewTicketPage = async () => {
   const user = await auth()
   if (!user) {
-    // Construct the absolute callbackUrl using the current request URL
-    const headersList = await headers()
-    const protocol    = headersList.get("x-forwarded-proto") || "https"
-    const host        = headersList.get("host")
-    const url         = `${protocol}://${host}/ticket/new`
+    const url         = await getAbsoluteUrl()
     const callbackUrl = encodeURIComponent(url)
     redirect(`${PATH_DIR.AUTH.sign_in}?callbackUrl=${callbackUrl}`)
   }
