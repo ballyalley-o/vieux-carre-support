9b17841089a7e1f654c4d6dbb44c4b43075b7420
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-04 10:55:35 +1000

    [debug]

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -27,6 +27,8 @@ export async function signAuthToken(payload: JWTPayload) {
 export async function verifyAuthToken<T>(token: string): Promise<T> {
     try {
         const { payload } = await jwtVerify(token, secret)
+        console.log('TOKEN at verifyAuthToken', token)
+        console.log('SECRET at verifyAuthToken', secret)
         return payload as T
     } catch (error: unknown) {
         const _errorMessage = transl('error.failed_decrypt_token')
